2905d07566e5e8791fc1ea2e77814a8ada545b62
Charles Atienza <catienza@webcreek.com>
2022-10-30 21:33:24 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GameComponent } from 'src/components/game/game.component';
+import { HeaderContentComponent } from 'src/components/main-menu/child-contents/header-content/header-content.component';
+import { MenuContentComponent } from 'src/components/main-menu/child-contents/menu-content/menu-content.component';
+import { MainMenuComponent } from 'src/components/main-menu/main-menu.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    MainMenuComponent,
+    MenuContentComponent,
+    HeaderContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/components/main-menu/child-contents/menu-content/menu-content.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-content',
+  templateUrl: './menu-content.component.html',
+  styleUrls: [
+    './menu-content.component.scss',
+    './css/neon-button.scss',
+    './css/neon-button-1.scss',
+    './css/neon-input.scss'
+  ]
+})
+export class MenuContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
