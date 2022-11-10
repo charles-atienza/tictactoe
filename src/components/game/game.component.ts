3ffb78d4e7ed666e20d8ac6e945aaf1910b0647e
charles.atienza <charles.atienza@yahoo.com>
2022-11-11 05:51:36 +0800

    local game logic completed

--- src/components/game/game.component.ts
+++ src/components/game/game.component.ts
@@ -1,4 +1,11 @@
+import { state } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+
+enum State {
+  blank = '',
+  x = 'x',
+  o = 'o'
+}
 
 @Component({
   selector: 'app-game',
@@ -7,32 +14,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  _currentPlayer = 'x';
+  boardSize: number = 3;
+  board: State[][] = [
+    [State.blank, State.blank, State.blank],
+    [State.blank, State.blank, State.blank],
+    [State.blank, State.blank, State.blank]
+  ];
+
+  moveCount = 0;
+
+  _currentPlayer = State.x;
 
   public get currentPlayer() {
     let cp = this._currentPlayer;
-    if (this._currentPlayer == 'x')
-      this._currentPlayer = 'x'
-    else {
-      this._currentPlayer = 'o'
-    }
+    this._currentPlayer = State.x
+
+    if (cp === State.x)
+      this._currentPlayer = State.o
 
     return cp;
   }
-
-  boardSize: string[] = [
-    '', '', '',
-    '', '', '',
-    '', '', '',
-  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onClick(index: number) {
-    console.log(this.boardSize)
-    this.boardSize[index] = this.currentPlayer;
+  onClick(x: number, y: number) {
+    if (this.board[x][y] !== State.blank) {
+      return;
+    }
+    let currentState = this.currentPlayer;
+    this.board[x][y] = currentState;
+
+    let checkForWinnerParam = {
+      x: x,
+      y: y,
+      s: currentState
+    }
+    this.checkForWinner(checkForWinnerParam);
+  }
+
+  checkForWinner(params: { x: number, y: number, s: State }) {
+    //check for a straight right
+    for (let i = 0; i < this.boardSize; i++) {
+      if (this.board[params.x][i] != params.s)
+        break;
+      if (i == this.boardSize - 1) {
+        console.log(`Winner is: ${params.s}`);
+        //report win for s
+      }
+    }
+
+    //check row
+    for (let i = 0; i < this.boardSize; i++) {
+      if (this.board[i][params.y] != params.s)
+        break;
+      if (i == this.boardSize - 1) {
+
+        console.log(`Winner is: ${params.s}`);
+        //report win for s
+      }
+    }
+
+    //check diag
+    if (params.x == params.y) {
+      //we're on a diagonal
+      for (let i = 0; i < this.boardSize; i++) {
+        if (this.board[i][i] != params.s)
+          break;
+        if (i == this.boardSize - 1) {
+
+          console.log(`Winner is: ${params.s}`);
+          //report win for s
+        }
+      }
+    }
+
+    //check anti diag (thanks rampion)
+    if (params.x + params.y == this.boardSize - 1) {
+      for (let i = 0; i < this.boardSize; i++) {
+        if (this.board[i][(this.boardSize - 1) - i] != params.s)
+          break;
+        if (i == this.boardSize - 1) {
+
+          console.log(`Winner is: ${params.s}`);
+          //report win for s
+        }
+      }
+    }
   }
 }
